61c5104232b90abd929875b78fabe17ee4046fad
민욱 <202596780+smart-rabbit-studio@users.noreply.github.com>
2025-05-24 23:22:57 +0900

    #첫번째 웹
    - Spring MavenProject 학습
    - 프로젝트 생성

---
+++ MavenProject/src/test/java/hellospring/mavenproject/MavenProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package hellospring.mavenproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
